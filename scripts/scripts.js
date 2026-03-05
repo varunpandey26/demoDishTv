a5e9c46eaab07a44847459df25f54837eb850614
Varun Pandey <varunpandey@dwao.in>
2026-03-05 12:14:08 +0530

    live-update+images-added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -187,3 +187,8 @@ async function loadPage() {
 }
 
 loadPage();
+
+(async function loadDa() {
+  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
+  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
+}());
